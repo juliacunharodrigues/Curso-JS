172477b12602f1cfe8ad82ec2e7cc243debd31ec
juliacunharodrigues <juliarodrigues.assaf@gmail.com>
2025-12-18 14:51:55 -0300

    aprendendo constante

---
+++ aula04/index.js
@@ -0,0 +1,12 @@
+const nome = 'joão' 
+
+const primeiroNumero = 5 //Number
+const segundoNumero = '10' //string
+const resultado = primeiroNumero * segundoNumero
+const resultadoDuplicado = resultado * 2
+let resultadoTriplicado = resultado * 3
+resultadoTriplicado = resultadoTriplicado + 5
+resultadoTriplicado = resultadoTriplicado + 20
+console.log(primeiroNumero)
+
+console.log(typeof (primeiroNumero + segundoNumero))
