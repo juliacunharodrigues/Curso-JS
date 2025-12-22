fbe090410d17df077a1ca17a1f9017098e33c20a
juliacunharodrigues <juliarodrigues.assaf@gmail.com>
2025-12-22 17:33:21 -0300

    operadores

---
+++ aula06/index.js
@@ -0,0 +1,23 @@
+/*const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2); */
+
+/*const num1 = '5';
+const num2 = 10;
+console.log(num1 + num2);*/
+
+/*let contador = 1
+contador++
+console.log(contador)*/
+
+/*let contador = 1 
+console.log(contador++) ESSA NAO É UMA BOA PRATICA*/
+
+/*let contador = 0;
+contador += 2;
+console.log(contador)*/
+
+const num1 = 20
+const num2 = Number('3.2')
+console.log(num1 + num2)
+console.log(typeof num2)
