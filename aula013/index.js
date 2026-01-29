94e4057756b685fe52c154a55e2649bca12a7487
juliacunharodrigues <juliarodrigues.assaf@gmail.com>
2026-01-29 18:25:25 -0300

    basico de funções

---
+++ aula013/index.js
@@ -0,0 +1,26 @@
+const raiz = function (n) {
+  return n ** 0.5; 
+}
+
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+// const raiz = n => n ** 0.5; 
+
+/*function soma(x = 1, y = 1){
+    const resultado = x + y;
+    return resultado; 
+    //console.log('Ola mundo!'); (nao é executado)
+}
+
+const resultado = soma(4, 9);
+console.log(resultado); */
+
+
+/*function saudacao(nome) {
+    return `Bom dia ${nome}!`
+}
+const variavel = saudacao('Júlia');
+console.log(variavel); */
