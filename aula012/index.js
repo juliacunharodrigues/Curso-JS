3c1183555ae4a340d7bad0272fd94cb4ecd39c11
juliacunharodrigues <juliarodrigues.assaf@gmail.com>
2026-01-29 16:16:08 -0300

    um pouco de arrays

---
+++ aula012/index.js
@@ -0,0 +1,27 @@
+const nomes = ['julia', 'maria', 'fred'];
+
+console.log(nomes instanceof Array);
+
+//nomes.push('mariana');
+//nomes.push('camila');
+//console.log(nomes.slice(0, -3))
+
+//const removido = nomes.shift();
+//console.log(removido);
+
+//nomes.unshift('salsicha');
+
+//nomes.push('joao');
+//nomes.push('mingau');
+
+
+//nomes[nomes.length] = 'luiza'; 
+//nomes[nomes.length] = 'alice';
+//console.log(nomes); 
+
+//nomes[3] = 'alice'
+//console.log(nomes)
+
+//nomes[1] = 'mingau'; 
+//console.log(nomes);
+//console.log(nomes[1]);
