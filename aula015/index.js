8ae0045c7edecec69f22172568a61d861ad34520
juliacunharodrigues <juliarodrigues.assaf@gmail.com>
2026-01-30 17:17:55 -0300

    valores

---
+++ aula015/index.js
@@ -0,0 +1,20 @@
+//primitivo
+
+/*let a = 'A';
+let b = a;
+console.log(a, b); //aponta o mesmo valor
+
+a = 'outra coisa';
+console.log(a, b); //aqui muda apenas o valor de a, o B ainda continua com 'A'.*/ 
+
+
+
+
+//referencia
+
+/*let a = [1, 2, 3];
+let b = a;
+console.log(a, b);
+
+a.push(4);
+console.log(b) //faz com que o b receba o mesmo valor de a, mesmo eu modificando apenas o a agora.*/ 
